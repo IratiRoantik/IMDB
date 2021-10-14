d01e199be9940e8b2425d61a3beb071da25457d3
Daniel Aramburu <aramburu.daniel.galeano@gmail.com>
2021-10-14 13:42:07 +0200

    Se ha añadido el archivo movie.ts

---
+++ movie.ts
@@ -0,0 +1,66 @@
+import { Professional } from "./professional";
+export class Movie{
+    public title:string;
+    public realeseYear:number;
+    public actors:Professional[]
+    public nationality:string;
+    public director:Professional;
+    public writer:Professional;
+    public language:string;
+    public plataform:string;
+    public isMCU:boolean;
+    public mainCharacterName:string;
+    public producer:string;
+    public distributor:string;
+    public genere:string;
+
+    constructor(title:string, realeseYear:number, nationality:string, genere:string){
+        this.title = title;
+        this.realeseYear = realeseYear;
+        this.nationality = nationality;
+        this.genere = genere;
+    }
+    
+    public printMovie():void{
+        let fullMovie:string = ``;
+        let pertenece:string = 'No';
+        if(this.isMCU){
+            pertenece = "Si"
+        }
+        fullMovie += `Titulo: ${this.title} \n
+        Año de estreno: ${this.realeseYear}\n
+        Actores que participan en la pelicula:` ;
+
+        for(let actor of this.actors){
+            let retirado:string = "No";
+            if(actor.isRetired){
+                retirado = "Si";
+            }
+            fullMovie +=`Nombre ${actor.name}\n
+            Edad: ${actor.age}\n
+            Genero: ${actor.genre}\n
+            Peso (Kg): ${actor.weight}\n
+            Altura(cm): ${actor.height}\n
+            Color de pelo: ${actor.hairColor}\n
+            Color de ojos: ${actor.eyeColor}\n
+            Raza: ${actor.race}\n
+            ` 
+         }
+
+        fullMovie += `Nacionalidad:  ${this.nationality}\n`;
+        fullMovie += `Director:  ${this.director}\n
+        Guionista:  ${this.writer}\n`;
+        fullMovie+=`Doblage original:  ${this.language}\n
+        Plataforma:  ${this.plataform}\n
+        Pertenece al universo cinematografico marvel?:  ${pertenece}\n
+        Personaje principal:  ${this.mainCharacterName}\n
+        Productor: ${this.producer}\n
+        Distribuidor: ${this.distributor}\n
+        Genero: ${this.genere}`;
+
+    }
+}
+
+
+// let pelicula1:Movie = new Movie("Escape Room",2019,"Estadounidense","Terror/Suspense");
+// pelicula1.printMovie()
