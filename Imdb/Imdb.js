070d28d93ea5c2492a4ae8369e675d9f52b85d52
IratiRoantik <irati.rantunez@gmail.com>
2021-10-15 11:50:05 +0200

    Se han creado los metodos para imdb.ts

--- Imdb/Imdb.js
+++ Imdb/Imdb.js
@@ -1,10 +1,20 @@
 "use strict";
 exports.__esModule = true;
 exports.Imdb = void 0;
+var fs = require("file-system");
 var Imdb = /** @class */ (function () {
     function Imdb(peliculas) {
         this.Movie = peliculas;
     }
+    Imdb.prototype.escribirEnJson = function (nombreDelFichero) {
+        var objetoAJson = JSON.stringify(this.Movie);
+        fs.writeFileSync(nombreDelFichero, objetoAJson);
+    };
+    Imdb.prototype.obtenerIntanciaIMDB = function (nombreDelFichero) {
+        var datos = fs.readFileSync(nombreDelFichero, { encoding: "utf8" });
+        var objectIMDB = JSON.parse(datos);
+        return objectIMDB;
+    };
     return Imdb;
 }());
 exports.Imdb = Imdb;
--- Imdb/Imdb.ts
+++ Imdb/Imdb.ts
@@ -1,11 +1,20 @@
 import { Movie } from '../classmovie/movie';
+import * as fs from "file-system";
 
-class Imdb{
+export class Imdb{
     public Movie : Movie[]
 
     constructor(peliculas : Movie [] ){
       this.Movie  = peliculas
     }
-
+    escribirEnJson(nombreDelFichero:string):void{
+      let objetoAJson = JSON.stringify(this.Movie)
+      fs.writeFileSync(nombreDelFichero, objetoAJson);
+    }
+    obtenerIntanciaIMDB(nombreDelFichero:string):Imdb{
+      let datos = fs.readFileSync(nombreDelFichero, {encoding:"utf8"})
+      let objectIMDB = JSON.parse(datos);
+      return objectIMDB;
+    }
 }
- export{Imdb}
+ 
